04b5e88f7f8fa19e45f8b60d63e9cba68d9d3e70
Sergei <sk-new@mail.ru>
2020-08-09 08:21:33 +0300

    view adjusted

--- app/src/main/java/com/skynet/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/skynet/cryptoapp/CoinPriceListActivity.kt
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : AppCompatActivity() {
+class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var coinViewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coin_price_list)
         coinViewModel = ViewModelProvider(this)[CoinViewModel::class.java]
 //        coinViewModel.priceList.observe(this, Observer {
 //            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
